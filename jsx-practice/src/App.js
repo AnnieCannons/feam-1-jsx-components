5d3d4cd2ea0db80c56c49c23f85d04b86e8c259e
lai_luv <l.lavaughn319@gmail.com>
2023-11-14 17:26:40 -0500

    done

--- jsx-practice/src/App.js
+++ jsx-practice/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import img1 from './Images/fall.jpg';
+import img2 from './Images/spring.jpg';
+import img3 from './Images/summer.jpg';
+import img4 from './Images/winter.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my seasonal website!</h1>
+      <div className='img_Container'>
+        <div className='card'>
+          <img src={img2}></img>
+          <p>Springtime</p>
+        </div>
+        <div className='card'>
+          <img src={img3}></img>
+          <p>Summertime</p>
+        </div>
+        <div className='card'>
+          <img src={img1}></img>
+          <p>Falltime</p>
+        </div>
+        <div className='card'>
+          <img src={img4}></img>
+          <p>Wintertime</p>
+        </div>
+      </div>
+      <div>
+        
+        <ul className='green'>
+          <h2>Some of my favorite seasonal activities are:</h2>
+          <li>Hiking in  the spring wildflowers</li>
+          <li>Surfing in the summer time</li>
+          <li>Playing i the fall leaves</li>
+          <li>Building snowmen in the winter </li>
+        </ul>
+      </div>
+
     </div>
   );
 }
